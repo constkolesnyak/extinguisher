a65aab87faf0a7c3d402dd76c4479e1da97f7f7c
Konstantin Kolesnyak <constkolesnyak@gmail.com>
2023-09-14 17:00:02 +0600

    deck: Add CARD_GROUPS

--- src/xshr/deck.py
+++ src/xshr/deck.py
@@ -1,8 +1,9 @@
+from collections import defaultdict
 from itertools import chain, product
 from pprint import pprint
 from random import SystemRandom
 
-from xshr.card import Card, Rank, Suit
+from xshr.card import Card, Mark, Rank, Suit
 
 _JOKERS_QUANTITY = 4
 JOKERS = set(
@@ -23,3 +24,16 @@ DECK = tuple(chain(map(Card.from_iterable, product(Rank, Suit)), JOKERS))
 
 def new_shuffled_deck() -> list[Card]:
     return SystemRandom().sample(DECK, len(DECK))
+
+
+def _init_card_groups() -> dict[Mark, set[Card]]:
+    card_groups: defaultdict[Mark, set[Card]] = defaultdict(set)
+
+    for _card in DECK[:-_JOKERS_QUANTITY]:
+        for _mark in _card.marks:
+            card_groups[_mark].add(_card)
+
+    return dict(card_groups)
+
+
+CARD_GROUPS: dict[Mark, set[Card]] = _init_card_groups()
