b504217f2e9cba516f90f94e67624a854677ff08
Konstantin Kolesnyak <constkolesnyak@gmail.com>
2023-09-14 17:59:24 +0600

    Add pile.py

---
+++ src/xshr/pile.py
@@ -0,0 +1,26 @@
+import operator
+from functools import reduce
+from pprint import pprint
+from typing import Iterator
+
+import attrs
+
+from xshr.card import Card, Mark, Suit
+from xshr.deck import CARD_GROUPS, JOKERS
+
+
+@attrs.define(frozen=True)
+class Pile:
+    _pile: frozenset[Card] = attrs.field(converter=frozenset)
+
+    @property
+    def jokers(self) -> frozenset[Card]:
+        return self._pile & JOKERS
+
+    def find(self, mark: Mark) -> frozenset[Card]:
+        if isinstance(mark, Suit):
+            return reduce(operator.or_, (CARD_GROUPS[suit] for suit in mark))
+        return self._pile & CARD_GROUPS[mark]
+
+    def __iter__(self) -> Iterator[Card]:
+        return iter(self._pile)
